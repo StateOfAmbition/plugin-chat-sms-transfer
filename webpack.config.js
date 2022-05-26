ab4bc2a5d42270df50d65fbae850183253ba2157
Alex Thomas <alex.c.thomas@gmail.com>
2022-05-26 17:01:32 +0100

    chore: upgrade plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,34 @@
+const DotEnvWebpack = require('dotenv-webpack');
 module.exports = (config, { isProd, isDev, isTest }) => {
-  /**
-   * Customize the webpack by modifying the config object.
-   * Consult https://webpack.js.org/configuration for more information
-   */
+    /**
+     * Customize the webpack by modifying the config object.
+     * Consult https://webpack.js.org/configuration for more information
+     */
+    // We dynamically change the path to the .env that contains the file corresponding to our profile
+    let envPath;
 
-  return config;
+
+    switch (process.env.TWILIO_PROFILE) {
+        case 'sidekick-stage':
+            envPath = '.env.stage';
+            break;
+        case 'sidekick-prod':
+            envPath = '.env.prod';
+            break;
+        default:
+          envPath = '.env.dev';
+
+    }
+    if(isDev) {
+        envPath = '.env.dev'
+    }
+    // If path was set, use the dotenv-webpack to inject the variables
+    console.log("envPath: ", envPath)
+    
+    if (envPath) {
+        config.plugins.push(new DotEnvWebpack({
+            path: envPath
+        }));
+    }
+    return config;
 }
